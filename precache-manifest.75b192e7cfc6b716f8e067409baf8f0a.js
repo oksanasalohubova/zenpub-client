9d2a817a77a27d20af882c5b17d7605d35a044d6
oksanasalohubova <oksanasalohubova@users.noreply.github.com>
2022-04-11 13:07:56 +0000

    deploy: 1851f604ea4ec73805c39fdc27978febc745afd5

---
+++ precache-manifest.75b192e7cfc6b716f8e067409baf8f0a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b70252f58972dc8164da65d11880e143",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0150141aa4f8ab2b63ab",
+    "url": "/static/css/3.02432556.chunk.css"
+  },
+  {
+    "revision": "5fbf1125b14be7c9f861",
+    "url": "/static/css/main.715072af.chunk.css"
+  },
+  {
+    "revision": "0150141aa4f8ab2b63ab",
+    "url": "/static/js/3.08de911e.chunk.js"
+  },
+  {
+    "revision": "026d7d0017202ac2555ebf4979d71b76",
+    "url": "/static/js/3.08de911e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1718aa149e1f1cc5a579",
+    "url": "/static/js/i18n-0.bb4aa799.chunk.js"
+  },
+  {
+    "revision": "5fbf1125b14be7c9f861",
+    "url": "/static/js/main.6c018bc8.chunk.js"
+  },
+  {
+    "revision": "9b333dbfb26774cce028",
+    "url": "/static/js/runtime-main.ee09dcfe.js"
+  },
+  {
+    "revision": "c1618ecf945923ee73d9201e19eb9321",
+    "url": "/static/media/messages.c1618ecf.po"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3bf6461b394dabddd55d11fb8cce8833.js"
+  "/precache-manifest.75b192e7cfc6b716f8e067409baf8f0a.js"
 );
 
 self.addEventListener('message', (event) => {
